f88d6b8d6c5a26bea53966d3e5845d0088aa60c6
Feigh <fey.vegborn@gmail.com>
2021-12-31 17:16:07 +0100

    Skapade huvudskärmen

--- EarwaxSuper/ClientApp/src/components/SoundPlayer.tsx
+++ EarwaxSuper/ClientApp/src/components/SoundPlayer.tsx
@@ -10,15 +10,13 @@ export class SoundPlayer extends React.PureComponent<{}, SoundPlayerProps> {
     constructor(props: any) {
         super(props);
     }
-    componentDidMount() {
 
-    }
     // Notering är att på grund av säkerhet stuff så låter chrome och etc inte en att autospela ljud
     // Lösningen är att köra PWA och installera den på windows datorn
     public render() {
         return (
             <div>
-                <img src="http://localhost/earwaxmedia/dog.jpg" ></img>
+                <img src="http://localhost/earwaxmedia/dog.jpg" alt="" ></img>
                 <audio id="audioId" autoPlay>
                     <source src="http://localhost/earwaxmedia/prezofbank.mp3" type="audio/mp3"></source>
                 </audio>
